b1d7b822e6722a261596aa13a74d6e1327aa4c6c
Ryan Willette <rbrickproductions123@gmail.com>
2020-02-03 23:01:48 -0600

    fix nil record

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ import (
 	"time"
 )
 
-const formatString = "❗*Coronavirus Updates*❗\n\n*Total Cases: %d \\(\\+%d\\)*\n*Total Deaths: %d \\(\\+%d\\)*\n*Last Updated: %s*"
+const (
+	formatDiffString   = "❗*Coronavirus Updates*❗\n\n*Total Cases: %d \\(\\+%d\\)*\n*Total Deaths: %d \\(\\+%d\\)*\n*Last Updated: %s*"
+	formatNoDiffString = "❗*Coronavirus Updates*❗\n\n*Total Cases: %d*\n*Total Deaths: %d*\n*Last Updated: %s*"
+	layout             = "Jan 2, 2006 @ 15:04"
+)
 
 var (
 	channelName  string
@@ -29,23 +33,29 @@ func poll() {
 	} else {
 		newRecord := source.records[0]
 		if bot != nil {
-			totalCasesDiff := newRecord.ConfirmedCases - recentRecord.ConfirmedCases
-			totalDeathsDiff := newRecord.Deaths - recentRecord.Deaths
+			text := ""
+			if recentRecord != nil {
+				totalCasesDiff := newRecord.ConfirmedCases - recentRecord.ConfirmedCases
+				totalDeathsDiff := newRecord.Deaths - recentRecord.Deaths
 
-			if totalCasesDiff != 0 || totalDeathsDiff != 0 {
-				msg := tgbotapi.MessageConfig{
-					BaseChat: tgbotapi.BaseChat{
-						ChannelUsername: fmt.Sprintf("@%s", channelName),
-					},
-					Text:      fmt.Sprintf(formatString, newRecord.ConfirmedCases, totalCasesDiff, newRecord.Deaths, totalDeathsDiff, newRecord.LastUpdated.Format("Jan 2, 2006 @ 15:04")),
-					ParseMode: "markdownv2",
+				if totalCasesDiff != 0 || totalDeathsDiff != 0 {
+					text = fmt.Sprintf(formatDiffString, newRecord.ConfirmedCases, totalCasesDiff, newRecord.Deaths, totalDeathsDiff, newRecord.LastUpdated.Format(layout))
 				}
-
-				if _, err := bot.Send(msg); err != nil {
-					log.Panicln(err)
-				}
+			} else {
+				text = fmt.Sprintf(formatNoDiffString, newRecord.ConfirmedCases, newRecord.Deaths, newRecord.LastUpdated.Format(layout))
 			}
 
+			msg := tgbotapi.MessageConfig{
+				BaseChat: tgbotapi.BaseChat{
+					ChannelUsername: fmt.Sprintf("@%s", channelName),
+				},
+				Text:      text,
+				ParseMode: "markdownv2",
+			}
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panicln(err)
+			}
 		}
 
 		recentRecord = newRecord
